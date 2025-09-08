aeda5a3ea41ae233a0666df1a169a4e332251076
CarolineOliveiraSilva <carolline1995@hotmail.com>
2025-09-08 13:53:28 -0300

    Atualizaçoes

--- sistema.c
+++ sistema.c
@@ -1,65 +1,60 @@
 #include <stdio.h>
 
 void exibirMenu(){
-    printf("\n ==== Sistema bancário ===\n");
+    printf("\n=== SISTEMA BANCÁRIO ===\n");
     printf("1. Consultar Saldo\n");
-    printf("2.Depositar\n");
+    printf("2. Depositar\n");
     printf("3. Sacar\n");
     printf("4. Sair\n");
     printf("Escolha uma opção: ");
-
 }
- 
+
 int main(){
 
-        int opcao;
-        float saldo = 1000.0;
-        float valor;
+    int opcao;
+    float saldo = 1000.0;
+    float valor;
 
     do{
         exibirMenu();
         scanf("%d", &opcao);
 
-        switch (opcao){ 
-
-        case 1:
-            printf("Seu saldo atual é: R$ %.2F\n", saldo);
+        switch (opcao)
+        {
+        case 1 :
+            printf("Seu saldo atual é: R$ %.2f\n", saldo);
             break;
-        
-        case 2:
-            printf("Digite o valor para deposito: R$ ");
+        case 2 :
+            printf("Digite o valor do depósito: R$ ");
             scanf("%f", &valor);
-            if(valor > 0 ){
+
+            if( valor > 0 ){
                 saldo += valor;
-                printf("deposito realizado! Novo saldo: R$ %2.f\n", saldo);
-
-            }else{
-                printf("Valor inválido!\n");
-        
-            }
-            break;
-        case 3:
-            printf("Digite o valor para saque: R$ ");
+                 printf("Depósito realizado! Novo saldo: R$ %.2f\n", saldo);
+                }else{
+                    printf("Valor invalido!\n");
+                }
+                break;
+        case 3 :
+            printf("Digite o valor do saque: R$ ");
             scanf("%f", &valor);
-            if(valor > 0 && valor <= saldo ){
+
+            if( valor > 0 && valor <=saldo ){
                 saldo -= valor;
-                printf("Saque realizado! Novo saldo: R$ %2.f\n", saldo);
-
-            }else{
-                printf("Valor inválido ou saldo insuficiente!\n");
+                 printf("Depósito realizado! Novo saldo: R$ %.2f\n", saldo);
+                }else{
+                    printf("Valor invalido ou saldo insuficiente!\n");
+                }
+                break;
+        case 4 :
+                printf("Obrigado por usar nosso sistema!");
+            break;
         
-            }
-            break;
-        case 4:
-             printf("Obrigado por usar nosso sistema!\n");
-            break;
-
-         default:
-         printf("Opção invalida! Tente novamente.\n");
-
+        default:
+        printf ("Opção inválida! tente novamente.\n");
         }
-    }while(opcao !=4);
+    }while(opcao != 4);
 
-return 0;
+    return 0;
 
 }
